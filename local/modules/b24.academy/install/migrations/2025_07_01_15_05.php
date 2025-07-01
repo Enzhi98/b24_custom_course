4171c055533dd9132b5b99c1a8da95fa42182695
Anzhela Khan <anzhela_khan@list.ru>
2025-07-01 16:54:05 +0400

    Добавление файлов для работы модуля

---
+++ local/modules/b24.academy/install/migrations/2025_07_01_15_05.php
@@ -0,0 +1,20 @@
+<?php
+
+use B24\Academy\UI\Tasks\ListPageMutator;
+use Bitrix\Main\Config\Option;
+
+CopyDirFiles(
+    dirname(__DIR__) . '/files/css',
+    $_SERVER['DOCUMENT_ROOT'] . '/local/css',
+    true,
+    true
+);
+
+    $em->registerEventHandler(
+        'main',
+        'OnEpilog',
+        'b24.academy',
+        ListPageMutator::class,
+        'handlerOnEpilog'
+    );
+Option::set('b24.academy', 'VERSION', 2025_07_01_15_05);
---
+++ local/modules/b24.academy/lang/ru/options.php
@@ -0,0 +1,3 @@
+<?php
+$MESS['B24_ACADEMY.GENERAL_TAB'] = 'Главное';
+$MESS['B24_ACADEMY.COMPANY_FACTS_IBLOCK_ID'] = 'Идентификатор инфоблока "Факты о компании"';
